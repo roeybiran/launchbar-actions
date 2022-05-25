fb410504d9a33c05dbbcbfb57f63644abd73fd62
roeybiran <roeybiran@icloud.com>
2022-05-25 12:32:24 +0300

    fix: pdf split

--- pdf-split.lbaction/Contents/Scripts/PDFSplitter/Sources/pdf_splitter/main.swift
+++ pdf-split.lbaction/Contents/Scripts/PDFSplitter/Sources/pdf_splitter/main.swift
@@ -1,26 +1,28 @@
 import Foundation
 import PDFKit
 
-CommandLine.arguments.dropFirst().forEach {
-  let sourceURL = URL(fileURLWithPath: $0)
+let paths = CommandLine.arguments.dropFirst().flatMap { (path: String) -> [String] in
+  let sourceURL = URL(fileURLWithPath: path)
 
   guard let document = PDFDocument(url: sourceURL) else {
     _ = NSAlert.runModal(.init(error: PDFError.invalidPDF(name: sourceURL.lastPathComponent)))
-    return
+    exit(0)
   }
 
   let basename = sourceURL.deletingPathExtension()
 
-  var paths = [String]()
-  for i in 0..<document.pageCount {
-    guard let pageData = document.page(at: i)?.dataRepresentation else { continue }
-    let targetName = basename.deletingPathExtension().lastPathComponent + "_\(i + 1)"
+  let paths: [String] = (0..<document.pageCount).compactMap { page in
+    guard let pageData = document.page(at: page)?.dataRepresentation else { return nil }
+    let targetName = basename.deletingPathExtension().lastPathComponent + "_\(page + 1)"
     let targetURL = basename.deletingLastPathComponent().appendingPathComponent(targetName).appendingPathExtension("pdf")
-    paths.append(targetURL.path)
     _ = PDFDocument(data: pageData)?.write(to: targetURL)
+    return targetURL.path
   }
-  print(paths.joined(separator: "\n"))
+
+  return paths
 }
+
+print(paths.joined(separator: "\n"))
 
 enum PDFError: LocalizedError {
   case invalidPDF(name: String)
