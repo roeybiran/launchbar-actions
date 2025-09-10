474c9d6d793cb6736fb2e40df78d3cc3629924fe
Roey Biran <roeybiran@icloud.com>
2025-09-10 21:13:49 +0300

    add credit

--- recognize-text.lbaction/Contents/Scripts/default.js
+++ recognize-text.lbaction/Contents/Scripts/default.js
@@ -1,5 +1,7 @@
 // LaunchBar Action Script
 
+// credit: https://www.raycast.com/huzef44/screenocr
+
 function run() {
     const fullScreen = LaunchBar.options.shiftKey
     let args = ["/usr/bin/env", "swift", `${Action.path}/Contents/Scripts/RecognizeText/Sources/main.swift`, fullScreen ? "--full-screen" : ""]
