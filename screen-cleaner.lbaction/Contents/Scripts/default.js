974fac0e7b946df9255df77a35d774317776b88b
Roey Biran <roeybiran@icloud.com>
2026-01-13 12:42:13 +0200

    Remove deprecated JavaScript script and update Info.plist to use shell script for default action

--- screen-cleaner.lbaction/Contents/Scripts/default.js
+++
@@ -1,6 +0,0 @@
-// LaunchBar Action Script
-
-function run(argument) {
-    let path = Action.path + "/Contents/Scripts/Screen Cleaner/Screen Cleaner/main.swift";
-    LaunchBar.execute("/usr/bin/swift", path);
-}
---
+++ screen-cleaner.lbaction/Contents/Scripts/default.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/opt/homebrew/bin/hs -c "StartScreenCleaner()" &>/dev/null
